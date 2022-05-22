f4b54faa0988b05ce2fda0d37b759c1a1794dfc5
zishan344 <zishanahmed344@gmail.com>
2022-05-22 16:40:50 +0600

    navbar update and carusel included

--- src/component/Shared/NotFound.js
+++ src/component/Shared/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 import notFound from "../../images/download.png";
 const NotFound = () => {
   return (
-    <div className="h-screen w-full">
+    <div className="h-screen w-full flex justify-center items-center">
       <img src={notFound} alt="" />
     </div>
   );
